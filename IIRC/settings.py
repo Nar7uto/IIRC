d34521cb4eb5bdf79b8e4291a749620a25d5a186
shah <shah@django.com>
2018-12-17 21:10:45 -0800

    complete the email just need a real email informations

--- IIRC/settings.py
+++ IIRC/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,41 +1,57 @@
 from django.shortcuts import render
 from .form import SignUpForm
 from .form import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 def home(request):
-	return render(request, 'home.html', {})
+    return render(request, 'home.html', {})
+
 
 def index(request):
-	
-	title = "Welcome guest"
-	form = SignUpForm(request.POST or None)
-	if request.user.is_authenticated:
-		title = "Welcome %s" %(request.user)
-	context = { 
-		'title':title,
-		'myName': 'Hasan',
-		'form':form,
-	}
-	if form.is_valid():
-		instance = form.save(commit=False)
-		if not instance.fullname:
-			email_base, provider = instance.email.split('@')
-			instance.fullname = email_base
-			instance.save()
-		context = { 
-			'title': 'Thank for Registeration',
-		}
-	return render(request, 'index.html', context)
+    title = "Welcome guest"
+    form = SignUpForm(request.POST or None)
+    if request.user.is_authenticated:
+        title = "Welcome %s" % (request.user)
+    context = {
+        'title': title,
+        'myName': 'Hasan',
+        'form': form,
+    }
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.fullname:
+            email_base, provider = instance.email.split('@')
+            instance.fullname = email_base
+            instance.save()
+        context = {
+            'title': 'Thank for Registeration',
+        }
+    return render(request, 'index.html', context)
 
 
 def contact(request):
-	form = ContactForm(request.POST or None)
-	if form.is_valid():
-		email = form.cleaned_data.get('email')
-		message = form.cleaned_data.get('message')
-		fullname = form.cleaned_data.get('fullname')
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form_email = form.cleaned_data.get('email')
+        form_message = form.cleaned_data.get('message')
+        form_fullname = form.cleaned_data.get('fullname')
 
-	context = {
-		'form': form,
-	}
-	return render(request, 'forms.html', context)
+        subject = 'Mail from Django'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email]
+        contact_message = '%s: %s via %s'%(
+            form_fullname,
+            form_message,
+            form_email)
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  to_email,
+                  fail_silently=False)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'forms.html', context)
